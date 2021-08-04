25a214cea264f4d3044a0ed7d7d624588ca88363
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-08-04 21:52:23 +0300

    startServer

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 
+const { envConstants: { PORT } } = require('./constants');
+
 const app = express();
 
-app.listen()
+app.listen(PORT, () => {
+  console.log(`App listen ${PORT}`);
+})
---
+++ server/constants/env.constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  ADMIN_EMAIL_PASSWORD: process.env.ADMIN_EMAIL_PASSWORD
+};
